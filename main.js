7e224908a33df2afba154ba829e39b12cc62a4f5
Jack Lach <jack.lach4@gmail.com>
2023-02-12 08:40:24 -0500

    all specifications satisfied. Added spec list.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 function init(){
   heartClick();
 }
@@ -51,3 +50,28 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+/*
+
+Note that the content of the "successful" response from the server is not important here — we only care that it's successful. This means you will not need to call .json() on the response so you only need a single then() call.
+
+Here's the specification:
+
+Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+When a user clicks on an empty heart:
+  Invoke mimicServerCall to simulate making a server request
+  When the "server" returns a failure status:
+    Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+    Display the error modal by removing the .hidden class
+    Display the server error message in the modal
+    Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+  When the "server" returns a success status:
+    Change the heart to a full heart
+    Add the .activated-heart class to make the heart appear red
+When a user clicks on a full heart:
+  Change the heart back to an empty heart
+  Remove the .activated-heart class
+Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
+Only manipulate the DOM once the server request responds. Do not make the heart full until you're inside a successful .then block.
+
+*/
